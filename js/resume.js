c09c60881fa604cea02a507d99d47d763f161c10
sudennings <sudennings@163.com>
2017-04-06 21:22:06 +0800

    添加slide

--- js/resume.js
+++ js/resume.js
@@ -1,21 +1,23 @@
 /**
  * Created by Administrator on 2017/3/24.
  */
+
 $(function(){
     /*------------banner---------*/
+    //banner切换图
     $('.banner-info').on("mouseover",function(){
             $(this).find("img").attr({src:"images/smile.jpg"});
 
     }).on("mouseout",function(){
         $(this).find("img").attr({src:"images/sad.jpg"});
     });
-//
-$(".wechat").click(function(){
-    $("#model").fadeIn(1000);
-    $("#weixin").slideDown();
-});
+    //点击出现微信二维码
+    $(".wechat").click(function(){
+        $("#model").fadeIn(1000);
+        $("#weixin").slideDown();
+    });
     $("#model").click(function(){
         $("#model").fadeOut(1000);
         $("#weixin").slideUp();
-    })
+    });
 });
